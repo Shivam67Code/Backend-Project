22d5ed2c58dfe85cb6325bafd535a889dd11d2bf
Shivam67Code <evilsniper64@gmail.com>
2025-09-19 18:08:32 +0545

    Fixed app import issue.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import dotenv from 'dotenv';
 import connectDB from "./db/db.js";
+import express from 'express';
+
+const app = express();
+
 dotenv.config({
-  path: './env'
+  path: './.env'  // Fixed: should be './.env' not './env'
 });
+
 connectDB()
   .then(() => {
-
     app.on("error", (error) => {
       console.log("Error : ", error);
       throw error;
-    })
+    });
+
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at https://localhost:${process.env.PORT}`);
-    })
+      console.log(`Server is running at http://localhost:${process.env.PORT || 8000}`);  // Fixed: http not https for local development
+    });
   })
   .catch((err) => {
     console.log("Mongo DB connection failed !!!", err);
-  })
+  });
