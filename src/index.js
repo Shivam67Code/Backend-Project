2e43b00392d6170bd20e31df3df567f087efb19c
Shivam67Code <evilsniper64@gmail.com>
2025-09-20 12:36:22 +0545

    Added Styled Default routes.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
 import connectDB from "./db/db.js";
 import express from 'express';
-
+import { defaultRoute, healthRoute } from './routes/defaultRoute.js';
 const app = express();
 
 dotenv.config({
-  path: './.env'  // Fixed: should be './.env' not './env'
+  path: './.env'
 });
 
 connectDB()
@@ -18,6 +18,9 @@ connectDB()
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running at http://localhost:${process.env.PORT || 8000}`);  // Fixed: http not https for local development
     });
+
+    app.get('/', defaultRoute);
+    app.get("/health", healthRoute);
   })
   .catch((err) => {
     console.log("Mongo DB connection failed !!!", err);
